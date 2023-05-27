e51b1329fb3bd7b75e7d6a7aef1365b07e6b243f
Avishay Halifa <96640086+t5avish@users.noreply.github.com>
2023-05-28 01:52:48 +0300

    Update tiles.py
    removed StaticTile and change Tiled class

--- code/tiles.py
+++ code/tiles.py
@@ -1,12 +1,8 @@
 import pygame
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, size, x, y):
+    def __init__(self, size, x, y, surface):
         super().__init__()
         self.image = pygame.Surface((size, size))
         self.rect = self.image.get_rect(bottomleft = (x, y))
-
-class StaticTile(Tile):
-    def __init__(self, size, x, y, surface):
-        super().__init__(size, x, y)
         self.image = surface
